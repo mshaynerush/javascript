14baf0a1f27071e702dbada4c4e7876922ae2045
Shayne Rushton <mshaynerush@gmail.com>
2019-04-05 15:38:00 -0700

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,6 +1,11 @@
+if ( document.readyState == 'loading) {
+    addEventListener('DOMContentLoaded', ready)
+}
+
+function ready(){
 var alertBtn = document.getElementsByClassName('alert-button')[0];
     alertBtn.addEventListener('click', doAlert);
-    
+}
 function doAlert(){
 alert('This is dope');
 }
